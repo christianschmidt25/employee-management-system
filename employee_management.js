0c0b87e5a32a4100bb1f8189f45efc230e54e54a
Christian Schmidt <cjschmidt33@gmail.com>
2024-09-27 22:18:14 -0400

    Create Manager class with inheritance from Employee

--- employee_management.js
+++ employee_management.js
@@ -31,3 +31,17 @@ class Department {
         return this.employees.reduce((total, employee) => total + employee.salary, 0); //brings all the employees salaries to one value, as a department total salary.
     }
 }
+
+
+// Task 3: Create a Manager Class that Inherits from Employee
+// Creates a new class, manager, and grabs the items from the employee class. It also includes the manager's bonus and new detail listing.
+
+class Manager extends Employee {
+    constructor(name, salary, position, department, bonus) {
+        super(name, salary, position, department);
+        this.bonus = bonus;
+    }
+    getDetails() {
+        console.log(`${this.name} works as a ${this.position} within the ${this.department} department making $${this.salary} per year. ${this.name} is receiving a bonus of $${this.bonus}.`);
+    }
+}
