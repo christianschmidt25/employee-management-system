e22fb0074d6c84bc417e8d659b8c5f60eac7eedd
Christian Schmidt <cjschmidt33@gmail.com>
2024-09-27 23:11:22 -0400

    Handle bonuses in the department salary calculation

--- employee_management.js
+++ employee_management.js
@@ -38,10 +38,27 @@ class Department {
 
 class Manager extends Employee {
     constructor(name, salary, position, department, bonus) {
-        super(name, salary, position, department);
+        super(name, salary, position, department); //gathers these items from the Employee class
         this.bonus = bonus;
     }
-    getDetails() {
+    getDetails() { //updates the details with an additional sentence for a bonus.
         console.log(`${this.name} works as a ${this.position} within the ${this.department} department making $${this.salary} per year. ${this.name} is receiving a bonus of $${this.bonus}.`);
     }
 }
+
+
+// Task 4: Handle Bonuses for Managers
+// This task will create a new salary for managers, including their bonuses.
+
+Department.prototype.calculateTotalSalaryWithBonus = function() { //goes back to find the Department class, and adds the function of calculating total salary with bonus
+    return this.employees.reduce((total, employee) => {
+        if (employee instanceof Manager) { //if employee is in the Manager class, it will do this function (and figure out the bonus)
+            return total + employee.salary + employee.bonus;
+        }
+
+        return total + employee.salary; //if employee is not a Manager (otherwise), it will just add their salary and nothing else
+    }, 0); //start the total salary at 0
+};
+    
+
+
